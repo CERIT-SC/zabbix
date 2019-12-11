6b1c7fb74566a3cf69536421838089e608f3b3fb
Marek <marekpastierik15@gmail.com>
2019-12-11 09:27:48 +0100

    Role server done

--- manifests/role/server.pp
+++ manifests/role/server.pp
@@ -6,7 +6,7 @@ class zabbix::role::server {
 
                 package {'zabbix':
                     provider => 'rpm',
-                    ensure   => installed,
+                    ensure   => "installed",
                     source   => 'https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm',
                 }
 
@@ -26,30 +26,23 @@ class zabbix::role::server {
        require => Package['zabbix'],
    }
    
-   file { 'mysql_credentials':
-       path    => "~/.my.cnf",
-       ensure  => "present",
-       content => epp(), #TODO
-       require => Package[$packages_to_install],
-   }
-
    exec { 'create zabbix db':
        subscribte  => Package[$packages_to_install],
-       command     => 'mysql -e "create database zabbix character set utf8 collate utf8_bin;"',
+       command     => '/bin/mysql -uroot -e "create database zabbix character set utf8 collate utf8_bin;"',
        refreshonly => true,
-       require     => File['mysql_credentials'],
+       require     => Package[$packages_to_install],
    }
 
    exec { 'grant privileges on zabbix':
-       subscribte  => Package[$packages_to_install],
-       command     => "mysql -e \"grant all privileges on zabbix.* to zabbix@localhost identified by \'${::zabbix::mysql_password}\'\";",
+       subscribte  => Exec['create zabbix db'],
+       command     => "/bin/mysql -uroot -e \"grant all privileges on zabbix.* to zabbix@localhost identified by \'${::zabbix::mysql_password}\'\";",
        refreshonly => true,
-       require     => [ File['mysql_credentials'], Exec['create zabbix db'] ],
+       require     => Exec['create zabbix db'],
    }
 
    exec { 'import initial schema':
-       subscribte  => Package[$packages_to_install],
-       command     => "zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u${::zabbix::mysql_username} -p${::zabbix::mysql_password}",
+       subscribte  => Exec[grant privileges on zabbix],
+       command     => "/bin/zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | /bin/mysql -uzabbix -p${::zabbix::mysql_password}",
        refreshonly => true,
        require     => Exec['grant privileges on zabbix'],
    }
@@ -58,12 +51,33 @@ class zabbix::role::server {
    file { 'zabbix config':
       ensure  => "present",
       path    => "/etc/zabbix/zabbix_server.conf",
-      content => epp(), #TODO
+      content => epp('/zabbix/zabbix_server.conf.epp', { "password" => $::zabbix::mysql_password }),
       require => Package[$packages_to_install],
    }
   
-   #TODO EDIT HTTPD conf.d
-   # START SERVICES
-   # update web/zabbix_server.conf.php
-}
+   file { "zabbix frontend":
+      ensure  => "present",
+      path    => "/etc/httpd/conf.d/zabbix.conf",
+      source  => 'puppet:///modules/zabbix/zabbix_php", 
+      require => Package[$packages_to_install], 
+   }
 
+   file { 'zabbix_php config':
+      ensure  => "present",
+      path    => "/etc/zabbix/web/zabbix.conf.php",
+      content => epp('zabbix/zabbix_server.php.epp', { "password" => $::zabbix::mysql_password }),
+      require => Package[$packages_to_install],
+   }
+
+   service { 'zabbix-server':
+      ensure  => "running",
+      enable  => true,
+      require => [ File['zabbix_php config'], File['zabbix config'] ],
+   }
+   
+   service { 'httpd':
+      ensure  => "running",
+      enable  => true,
+      require => File['zabbix frontend'],
+   }
+}
