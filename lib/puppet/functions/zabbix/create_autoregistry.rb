6dd356f9a0b7539af147087255e45dab228bbd66
Marek <marekpastierik15@gmail.com>
2019-12-12 10:51:51 +0100

    Update function to create auto_registry

--- lib/puppet/functions/zabbix/create_autoregistry.rb
+++ lib/puppet/functions/zabbix/create_autoregistry.rb
@@ -3,8 +3,8 @@ require 'json'
 
 Puppet::Functions.create_function(:'zabbix::create_autoregistry') do
 
-      GENERIC_PAYLOAD = { "jsonrpc" => "2.0", "id" => 1}
-      URL = ""
+      $generic_payload = { "jsonrpc" => "2.0", "id" => 1}
+      $url_g = ""
 
       dispatch :create_autoregistry do
          param 'Array',  :templates
@@ -15,8 +15,8 @@ Puppet::Functions.create_function(:'zabbix::create_autoregistry') do
 
       def create_autoregistry(templates, url, apiKey)
          url += "/api_jsonrpc.php"
-         URL = url
-         GENERIC_PAYLOAD.merge({"auth" => apiKey})
+         $url_g = url
+         $generic_payload = $generic_payload.merge({"auth" => apiKey})
 
          ids = getIdOfTemplates(templates)
          attributes = { "method" => "action.create", "params" => { "name" => "Auto registration", "eventsource" => 2, "operations" => [ { "operationtype" => 2 }, { "operationtype" => 6, "optemplate" => ids }]}}
@@ -24,18 +24,18 @@ Puppet::Functions.create_function(:'zabbix::create_autoregistry') do
       end
 
 
-      def getIdOfTemplates(templates, url)
-         arguments = { "method" => "template.get", "params": {"filter": { "host": templates } }}
+      def getIdOfTemplates(templates)
+         arguments = { "method" => "template.get", "params" => {"filter" => { "host" => templates } }}
          result = genericHttpGet(arguments)
-         result.map { |template| { "templateid" : "#{template['templateid']}"} }
+         result.map { |template| { "templateid" => "#{template['templateid']}"} }
          return result
       end
 
 
       def genericHttpGet(payload)
-         payload.merge(GENERIC_PAYLOAD)
+         payload = payload.merge($generic_payload)
          begin
-            result = RestClient::Request.execute(:url => URL, :method => "GET", :verify_ssl => false, :timeout => 10, :payload => payload.to_json, :headers => { "Content-Type" => "application/json"})
+            result = RestClient::Request.execute(:url => $url_g, :method => "GET", :verify_ssl => false, :timeout => 10, :payload => payload.to_json, :headers => { "Content-Type" => "application/json"})
          rescue
             return []
          end
@@ -44,9 +44,9 @@ Puppet::Functions.create_function(:'zabbix::create_autoregistry') do
 
 
       def genericHttpPost(payload)
-         payload.merge(GENERIC_PAYLOAD)
+         payload = payload.merge($generic_payload)
          begin
-            result = RestClient::Request.execute(:url => URL, :method => "POST", :verify_ssl => false, :timeout => 10, :payload => payload.to_json, :headers => { "Content-Type" => "application/json"})
+            result = RestClient::Request.execute(:url => $url_g, :method => "POST", :verify_ssl => false, :timeout => 10, :payload => payload.to_json, :headers => { "Content-Type" => "application/json"})
          rescue
             return
          end
