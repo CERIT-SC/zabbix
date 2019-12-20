0a50cd1e589b094a19b1a6d6be384e5df3c4fda5
Marek <marekpastierik15@gmail.com>
2019-12-20 09:25:24 +0100

    Create role for pbs

---
+++ manifests/role/pbs_node.pp
@@ -0,0 +1,22 @@
+class zabbix::role::pbs_node (
+  String $name_of_check = "pbs",
+  String $command,
+) {
+
+  $pbs_check_zabbix = @(END)
+  HostMetadata = pbs_node
+  UserParameter=<%= $name_of_check %>, <%= $command %>
+  END
+
+  file { '/etc/zabbix/zabbix_agentd.d':
+     ensure => 'directory',
+  }
+
+  file { 'PBS check enable':
+     ensure  => "present",
+     content => inline_epp($pbs_check_zabbix, {'name' => $name_of_check, 'command' => $command })
+     path    => '/etc/zabbix/zabbix_agentd.d/pbs.conf',
+     notify  => Service['zabbix-agent'],
+     require => File['/etc/zabbix/zabbix_agentd.d'],
+  }
+}
