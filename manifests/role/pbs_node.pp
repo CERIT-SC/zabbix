7fb13a64249a55f6afecdedc439335b9d2e7755a
Marek <marekpastierik15@gmail.com>
2019-12-20 09:52:45 +0100

    typo

--- manifests/role/pbs_node.pp
+++ manifests/role/pbs_node.pp
@@ -3,21 +3,15 @@ class zabbix::role::pbs_node (
   String $command,
 ) {
 
-  $pbs_check_zabbix = @(END)
-  HostMetadata = pbs_node
-  UserParameter=<%= $name_of_check %>, <%= $command %>
-  END
-
   file { '/etc/zabbix/zabbix_agentd.d':
      ensure  => 'directory',
-     require => Class['zabbix::role::agent'],
+     require => File['zabbix-agent-config'],
   }
 
   file { 'PBS check enable':
      ensure  => "present",
-     content => inline_epp($pbs_check_zabbix, {'name' => $name_of_check, 'command' => $command })
+     content => inline_epp("HostMetadata=pbs_node\nUserParameter=$name_of_check,$command", {'name' => $name_of_check, 'command' => $command }),
      path    => '/etc/zabbix/zabbix_agentd.d/pbs.conf',
      notify  => Service['zabbix-agent'],
-     require => File['/etc/zabbix/zabbix_agentd.d'],
   }
 }
